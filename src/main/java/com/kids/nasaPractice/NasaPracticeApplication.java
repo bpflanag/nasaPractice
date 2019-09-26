4179c5e3a72d469fdb3ad2d6b1cef7d100b7847b
Brian Flanagan <brianflanagan@Brians-MacBook-Pro.local>
2019-09-26 18:52:38 -0400

    Initial Commit

---
+++ src/main/java/com/kids/nasaPractice/NasaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kids.nasaPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaPracticeApplication.class, args);
+	}
+
+}
